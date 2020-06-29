9dd346aac24d81b0805e6a73d3135324f99ccf78
sang-pan <67272865+sang-pan@users.noreply.github.com>
2020-06-29 15:36:11 +0800

    wsdc-background-sangpan
    后台会员管理的实现

--- userControl.php
+++ userControl.php
@@ -1,131 +1,98 @@
 <?php
-header("content-type:text/html;charset=utf-8");
-$_GET['act']=isset($_GET['act'])?$_GET['act']:"login";
+$_GET['act']=isset($_GET['act'])?$_GET['act']:"list";
 switch($_GET['act']){
-	case "login":
-		?>
-		<script>
-		function checkForm(){
-		  if(!$('input[name=username]').val()){alert('账号不能为空');$('input[name=username]').focus();return false;}
-		  if(!$('input[name=password]').val()){alert('密码不能为空');$('input[name=password]').focus();return false;}
+	case "list":
+		echo "<form style='padding:0px;margin:0px;' action='admin.php?mod=user&act=list' method='post'>
+		<span class='status'>&nbsp;&nbsp;会员名称&nbsp;&nbsp;&nbsp;&nbsp;</span><input name='username' value='{$_REQUEST['username']}' style='padding:0px;margin:0px;'>
+		<input type='submit' value='搜索'> <input type='button' onclick='location=\"admin.php?mod=user&act=reg\"' value='注册'>
+		</form>";   
+		$fsql="";$fpage="";
+		if(isset($_REQUEST['username'])&&$_REQUEST['username']){
+			$fsql.=" and username like '%{$_REQUEST['username']}%'";
+			$fpage="&username={$_REQUEST['username']}";
+		}  
+		$countsql="select count(*) from user where 1=1 $fsql";
+		$pagesql="select * from user where 1=1 $fsql order by tag desc";
+		$bottom="?action=list";
+		$datasql=page($countsql,$pagesql,$bottom,15);
+		echo "<form name='delform' id='delform' action='?mod=user&act=alldel' method='post' class='margin0'>
+		<table style='width:98%;' align='center' border='1'>
+		<tr  height='30' align='center'><td>账号</td><td>类型</td><td>管理</td></tr>";
+		if($datasql){
+			while($rs=fetch($datasql[1])){
+			  echo "<tr height='20'  bgcolor='#FFFFFF'>
+			  <td align='left'><input   type=checkbox value='{$rs['id']}'  name='allidd[]' id='allidd'>{$rs['username']}</td>
+			  <td align='center'>";switch($rs['tag']){case 8:echo "管理员";break;case 6:echo "会员";break;}echo"</td>
+			  <td align='center'>		  
+				<a href='admin.php?mod=user&act=edit&id={$rs['id']}'>编辑</a>  &nbsp; &nbsp;
+				<a href='javascript: if(confirm(\"您确定要删除该会员吗？\")) location=\"admin.php?mod=user&act=alldel&id={$rs['id']}\" '>删除</a> 
+			  </td>
+			  </tr>";
+			}
+			echo "<tr><td colspan=5 align='right'>
+					 <div style='width:280px;float:left'>{$datasql['pl']}{$datasql['pldelete']}</div>
+					 <div  style='float:right'>{$datasql[2]}</div>
+					 <div style='clear:both;'></div>
+			  </td></tr>";
 		}
-		</script>    
-		<div style='margin:auto;width:300px;margin-top:50px;' align='left'>
-		<b>请先登录</b>
-		<form class='myform' action='index.php?mod=user&act=logingo' method='post' onsubmit='return checkForm()' style='width:350px;'>
-		<span class='myspan' style='width:120px;'>会员账号：</span><input name='username' style='width:150px;'><br><br>
-		<span class='myspan' style='width:120px;'>登录密码：</span><input name='password' type='password' style='width:150px;'><br><br>
-		<center> <input type='submit' value='登录' class='submit'> <input type='button'  onclick="location='index.php?mod=user&act=reg'" value='注册' class='submit'> </center>
-		</form>
-		</div>
-		<?php
-	break;
-	
-	case "logingo":
-		$row=getone("select * from user where username='{$_POST['username']}' and password='".md5($_POST['password'])."'");
-		if($row['id']){
-		  if($row['tag']==6){
-			$_SESSION['username']=$row['username'];
-			$_SESSION['userid']=$row['id'];
-			$_SESSION['password']=$row['password'];
-			$_SESSION['goods']=array();
-			echo "<script>location='index.php?mod=goods&act=list'</script>";
-		  }
-		  elseif($row['tag']==8){
-			$_SESSION['adminname']=$row['username'];
-			$_SESSION['adminid']=$row['id'];
-			$_SESSION['adminpassword']=$row['password'];
-			$_SESSION['goods']=array();
-			echo "<script>location='admin.php'</script>";
-		  }
-		}
-		else {echo "<script>alert('用户名或密码错误！');location='index.php?mod=user&act=login'</script>";}	
-	break;
-	
-	case "out":
-		if(isset($_SESSION['username'])){
-			$_SESSION['username']="";
-			$_SESSION['userid']="";
-			$_SESSION['password']="";
-			unset($_SESSION['username'],$_SESSION['userid'],$_SESSION['password']);
-			echo "<script>location='index.php?mod=user&act=login'</script>";
-		}
-		if(isset($_SESSION['adminname'])){
-			$_SESSION['adminname']="";
-			$_SESSION['adminid']="";
-			$_SESSION['adminpassword']="";
-			unset($_SESSION['username'],$_SESSION['userid'],$_SESSION['password']);
-			echo "<script>location='index.php?mod=user&act=login'</script>";
-		}
-	break;
-	
-	case "edit":
-		checkLoginStatus();
-		?>
-		<script>
-		function checkForm(){
-		  if(!$('input[name=oldpassword]').val()){alert('原密码不能为空');$('input[name=oldpassword]').focus();return false;}
-		  if(!$('input[name=password]').val()){alert('新密码不能为空');$('input[name=password]').focus();return false;}
-		}
-		</script>
-		<div style='width:430px;height:230px;margin-top:20px;margin:120px auto;' align='left'>
-		<form class='myform' action='index.php?mod=user&act=editgo' method='post' onsubmit='return checkForm()' >
-		<span class='myspan' style='width:70px;'>原密码：</span><input name='oldpassword'   style='width:150px;'><br><br>
-		<span class='myspan' style='width:70px;'>新密码：</span><input name='password' type='password' style='width:150px;'><br><br>
-		<center> <input type='submit' value='保存' class='submit'></center>
-		</form>
-		</div>
-		<?php
-	break;
-	
-    case "editgo":
-		checkLoginStatus();
-		$U=getone("select * from user where id='{$_SESSION['userid']}'"); 
-		if($U['password']!=md5($_POST['oldpassword'])){
-		   echo "<script>alert('原密码不正确');location='?mod=user&act=edit'</script>";
-		   die();
-		}
-		else if(query("update user set password='".md5($_POST['password'])."' where id='{$_SESSION['userid']}'")){
-		   echo "<script>alert('编辑成功');location='?mod=user&act=edit'</script>";
-		   die();
-		}
+		echo "</table></form>";
 	break;
 	
 	case "reg":
-		?>
+	    ?>
 		<script>
-		function checkForm(){
-		  if(!$('input[name=username]').val()){alert('用户名不能为空');$('input[name=username]').focus();return false;}
+		function check(){
+		  if(!$('input[name=username]').val()){alert('会员名称不能为空');$('input[name=username]').focus();return false;}
 		  if(!$('input[name=password]').val()){alert('密码不能为空');$('input[name=password]').focus();return false;}
 		}
 		</script>
-		<div style='width:430px;height:230px;margin-top:20px;margin:120px auto;' align='left'>
-		<form class='myform' action='index.php?mod=user&act=save' method='post' onsubmit='return checkForm()' >
-		<span class='myspan' style='width:70px;'>用户名：</span><input name='username'   style='width:150px;'><br><br>
-		<span class='myspan' style='width:70px;'>密码：</span><input name='password' type='password' style='width:150px;'><br><br>
-		<span class='myspan' style='width:70px;'>年龄：</span><input name='age'   style='width:150px;'><br><br>
-		<span class='myspan' style='width:70px;'>性别：</span><select name='gender'   style='width:150px;'><option>男</option><option>女</option></select><br><br>
-		
-		<center> <input type='submit' value='注册' class='submit'></center>
-		</form>
-		</div>
+		<center>
+		<div style='width:420px;height:230px;margin-top:20px;' align='left'>
+		<form action='?act=save' method='post'  onsubmit='return check()' class='myform'>
+		账号：<input name='username' style='width:160px;'><br>
+		密码：<input name='password' type='password' style='width:160px;'><br> 
+		年龄：<input name='age' style='width:160px;'><br>
+		性别：<input name='gender' style='width:160px;'><br> 
+		类型：<select name='tag'><option value=8>管理员</option><option value=6>会员</option></select><br> 
+		<center><input type='submit' value='注册' class='submit'></center></form>
+		</div></center>
 		<?php
 	break;
-	
-	case "save":
-		$finde=@getone("select * from user where username='{$_POST['username']}'");
-		if($finde['id']){
-		 echo "<script>alert('该用户名已存在，请重新注册');location='index.php?mod=user&act=reg'</script>";
-		 die();
+	  	  
+	case "edit":
+		$id=intval($_GET['id']);
+		$Arr=getone("select * from user where id=$id");
+		echo "
+		<script>
+		function check(){
+		  if(!$('input[name=username]').val()){alert('会员名称不能为空');$('input[name=username]').focus();return false;}	   
 		}
-		if(query("insert into  user set 
-		username='{$_POST['username']}',
-		password='".md5($_POST['password'])."',
-		gender='{$_POST['gender']}',
-		age='{$_POST['age']}',
-		tag='6'
-		"));
-		echo "<script>alert('注册成功，请登录！');location='index.php?mod=user&act=login'</script>";
-	break;	
+		</script>
+		<center>
+		<div style='width:420px;height:230px;margin-top:20px;' align='left'>
+		<form action='?act=save&id=$id' method='post' class='myform' onsubmit='return check()'>
+		账号：<input name='username' value='{$Arr['username']}' style='width:160px;'><br>
+		密码：<input name='password' type='password' style='width:160px;'><br> 
+		年龄：<input name='age' value='{$Arr['age']}' style='width:160px;'><br>
+		性别：<input name='gender'  value='{$Arr['gender']}' style='width:160px;'><br> 
+		
+		类型：<select name='tag'>
+		<option value=8>管理员</option>
+		<option value=6 ";if($Arr['tag']==6)echo" selected";echo">会员</option>
+		</select>
+		<center><input type='submit' value='保存' class='submit'></center></form>
+		</div></center>";
+	break;
+
+	case "alldel":
+	  	$key=isset($_POST["allidd"])&&$_POST["allidd"]?$_POST["allidd"]:array(intval($_GET['id']));
+	    for($i=0;$i<count($key);$i++){ 
+		    $id=$key[$i];
+		    $find=getone("select tag from user where id=$id");
+
+			query("delete from user where id=$id");
+	    }	
+		echo "<script>alert('删除成功!');location='?act=list'</script>";
+	break;
 }
 ?>
